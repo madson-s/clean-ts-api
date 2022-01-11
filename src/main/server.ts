6c2ff2bb0e773a2a73c92f142c0613e27b7b4945
Madson Silva <madsonoliveirasilva@gmail.com>
2022-01-10 21:57:11 -0300

    refactor: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,7 @@
-import app from './config/app'
+import { MongoHelper } from '../infra/db/mongodb/account-repository/helper/mongo-helper'
+import env from './config/env'
 
-app.listen(5050, () => console.log('🔥 Server running at http://localhost:5050'))
+MongoHelper.connect(env.mongoUrl).then(async () => {
+  const app = (await import('./config/app')).default
+  app.listen(env.port, () => console.log('🔥 Server running at http://localhost:5050'))
+}).catch(console.error)
